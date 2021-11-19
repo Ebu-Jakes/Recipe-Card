4b4123a35e38dbbc5757a6877c92abfd3b4e0d4c
Ebu-Jakes <ebujakes@gmail.com>
2021-11-19 13:26:12 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary:'#FF6363',
+        secondary:{
+          100:'#E2E2D7',
+          200:'#888882',
+        },
+        headcol:'#BE6531',
+        paracol:'#AFC15B'
+      },
+      fontFamily:{
+        pop:['Poppins',],
+        nut:['Nunito']
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
